a90480d71491f254cf8b2c59db4e3db7b945c71e
marciojesusmaia <marciojesusmaia@hotmail.com>
2024-09-14 13:29:29 -0300

    primeiros passos do game

---
+++ adivinhacao.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+	printf("************************************\n");	
+	printf("* Bem vindo ao jogo de adivinhação *\n");
+	printf("************************************\n");
+	
+	int numsec = 42;
+	int chute, erros;
+	erros = 0;
+	
+	printf("Qual o seu chute?\n");
+	scanf("%d", &chute)
+		
+	if (chute == numsec)
+		printf("** Parabens, você acertou! **");
+	else if(chute > numsec)
+		printf("** Você chutou %d! Eu pensei em um numero menor! **", chute);
+	else
+		printf("** Você chutou %d! eu pensei em um numero menor! **", chute);
+}

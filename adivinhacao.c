83b8258280596159a7fd51213ff5725e3b825111
marciojesusmaia <marciojesusmaia@hotmail.com>
2024-09-19 07:29:13 -0300

    add erro e tentativa com while

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TENTATIVAS 3
+
+
+
+
 int main (){
+	int numsec = 42;
+	int pontos = 1000;
+	int chute, erros, tentativas, scorep;
 	
 	printf("************************************\n");
 	printf("*                                  *\n");
@@ -10,39 +16,34 @@ int main (){
 	printf("*                                  *\n");
 	printf("************************************\n");
 	
-	int numsec = 42;
-	int chute, erros, tentativas;
-	erros = 0;
-	tentativas =1;
-	//menor = chute < numsec;
-	
-	
+		erros = 0;
+		tentativas = 1;
 	while (chute != numsec){
-	printf("Qual o seu %d chute?\n", tentativas);
-	scanf("%d", &chute);
-		if (chute < 0){
-			printf("Somente numeros positivos!!\n");
-			exit(1);
-		}
+
+		printf("Qual o seu %d chute?\n", tentativas);
 		
-		if (chute == numsec){
-			printf("\n** Parabens, você acertou! **\n");
-					}
-	
-		else {
-			if(chute > numsec){
-				printf("Você chutou um numero maior! tente novamente...\n");
-				erros++;
-					}
-		else{
-				printf("Você chutou um numero menor! tente novamente...\n");
-				erros++;
+		scanf("%d", &chute);
+		
+			if (chute < 0){
+				printf("Somente numeros positivos!!\n");
+				exit(1);
 			}
+
+			if (chute == numsec){
+				printf("\n** Parabens, você acertou! e teve %d erros **\n", erros);
+				printf("-- Fim do jogo! --\n");
+				break;
+						}
+
+			else if (chute > numsec)
+				printf("seu chute foi maior!\n");
+							 	
+			else
+				printf("Você chutou um numero menor! tente novamente...\n");
+				
+			tentativas++;
+			erros++;
+			}
+			
 		}
-		tentativas++;
-	}
-	printf("** Você teve %d erros! **\n\n", erros);
-	printf("-- Fim do jogo! --\n");
-	
-	}
 	

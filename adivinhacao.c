58f3035fee5f05eca9d8024ac261e28b93bac7e5
marciojesusmaia <marciojesusmaia@hotmail.com>
2024-09-21 07:46:12 -0300

    implementado mensagens e melhorado sistema de niveis

--- adivinhacao.c
+++ adivinhacao.c
@@ -21,17 +21,34 @@ int main (){
 	printf("*                                  *\n");
 	printf("* Bem vindo ao jogo de adivinhação *\n");
 	printf("*                                  *\n");
+	printf("*            ___                   *\n");
+	printf("*           |__ \\                  *\n");
+	printf("*             / /                  *\n");
+	printf("*            |_|                   *\n");
+	printf("*            (_)                   *\n");
 	printf("************************************\n\n");
+	printf("** Você deve adivinhar um numero entre 0 e 100 **\n\n");
 	printf("Qual o nivel de dificuldade você escolhe? \n");
 	printf("(1)Facil (2)Medio (3)Dificil\n");
 	printf("Escolha: ");
 	scanf("%d", &nivel);
-	if(nivel == 1)
-		   rodadas = 20;
-	else if(nivel == 2)
-		   rodadas = 15;
-	else
-		   rodadas = 6;
+	printf("\n");
+	
+	
+	switch(nivel){
+		case 1:
+		rodadas = 15;
+			printf("No nivel fácil voce terá %d rodadas\n", rodadas);
+		break;
+		case 2:
+		rodadas = 10;
+			printf("No nivel médio voce terá %d rodadas\n", rodadas);
+		break;
+		case 3:
+		rodadas = 5;
+			printf("No nivel difícil voce terá somente %d rodadas\n", rodadas);
+		break;
+	}
 	
 		
 	for(int i=1; i<=rodadas;i++){
@@ -54,15 +71,15 @@ int main (){
 						break;
 								}
 					else if (i == rodadas){
-						printf("Voce perdeu!!\n");
+						printf("Que pena,você perdeu!!\nObrigado por jogar!\n");
 						break;
 					}
 
 					else if (chute > numsec)
-						printf("seu chute foi maior!\n");
+						printf("seu chute foi MAIOR!\nTente novamente...\n");
 
 					else
-						printf("Você chutou um numero menor! tente novamente...\n");
+						printf("Você chutou um numero MENOR!\nTente novamente...\n");
 
 					tentativas++;
 					erros++;
